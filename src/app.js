7bb2bba4ba49035cacac1d4b173cc8699ab7e799
Humnashin Khan <humnashin9955@gmail.com>
2025-06-23 13:21:32 +0530

    install cors and fixed cors issue

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
-
+const cors = require("cors");
 //Middleware and it is activated for all the routes.
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));    // it will stop giving me cors error.
 app.use(express.json());
 app.use(cookieParser());
 
